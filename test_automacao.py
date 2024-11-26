5456d70920970866c55d43bbad6e83ec0dad6688
ianrxdrigues <ianrodrigues620@gmail.com>
2024-11-26 19:30:28 -0300

    Update test_automacao.py

--- test_automacao.py
+++ test_automacao.py
@@ -10,6 +10,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from threading import Thread
 from socketio import Client
+import logging
+
+# Configurar logging para debugar melhor
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Inicializar o cliente SocketIO para comunicar com o servidor
 sio = Client()
@@ -21,16 +26,16 @@ def configurar_navegador():
     options.add_argument("--ignore-ssl-errors")
     options.add_argument("--disable-notifications")
     options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--remote-debugging-port=9222")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-infobars")
-    options.add_argument("--disable-setuid-sandbox")
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--mute-audio")
+    options.add_argument("--headless")  # Necessário para rodar sem interface gráfica
+    options.add_argument("--no-sandbox")  # Importante para evitar problemas no Heroku
+    options.add_argument("--disable-dev-shm-usage")  # Reduz a utilização de memória compartilhada
+    options.add_argument("--disable-gpu")  # Desabilita GPU, importante para rodar sem interface gráfica
+    options.add_argument("--remote-debugging-port=9222")  # Adiciona suporte para depuração remota
+    options.add_argument("--disable-extensions")  # Desativa extensões para evitar falhas
+    options.add_argument("--disable-infobars")  # Desativa a barra de informações do Chrome
+    options.add_argument("--disable-setuid-sandbox")  # Para evitar problemas de permissões
+    options.add_argument("--window-size=1920,1080")  # Define um tamanho fixo para a janela
+    options.add_argument("--mute-audio")  # Desativa o áudio para evitar desconforto
     options.add_argument(
         "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
     )
@@ -41,9 +46,9 @@ def configurar_navegador():
 def carregar_cookies(driver, cookies_file):
     try:
         driver.get("https://www.tiktok.com")
-        time.sleep(3)  # Menor tempo para reduzir espera
+        time.sleep(3)
 
-        print(f"Carregando cookies do arquivo: {cookies_file}")
+        logger.info(f"Carregando cookies do arquivo: {cookies_file}")
         with open(cookies_file, "r", encoding="utf-8") as file:
             cookies = json.load(file)
 
@@ -60,69 +65,69 @@ def carregar_cookies(driver, cookies_file):
                     cookie["domain"] = ".tiktok.com"
 
                 driver.add_cookie(cookie)
-                print(f"Cookie adicionado: {cookie}")
+                logger.info(f"Cookie adicionado: {cookie}")
 
         driver.get("https://www.tiktok.com")
-        time.sleep(3)  # Reduzido para 3 segundos
+        time.sleep(3)  # Esperar o carregamento da página
 
         # Verifica se a página inicial não possui um link de login
         page_source = driver.page_source
         if "Login" in page_source or "Entrar" in page_source or "Sign Up" in page_source:
             raise Exception("Login não detectado. Verifique os cookies fornecidos.")
         else:
-            print("Login realizado com sucesso.")
+            logger.info("Login realizado com sucesso.")
     except ValueError as ve:
-        print(f"Erro no formato dos cookies: {ve}")
+        logger.error(f"Erro no formato dos cookies: {ve}")
     except Exception as e:
-        print(f"Erro ao carregar cookies do arquivo {cookies_file}: {e}")
+        logger.error(f"Erro ao carregar cookies do arquivo {cookies_file}: {e}")
 
 def comentar_no_tiktok(driver, url, comentario):
     try_count = 0
-    max_retries = 2  # Reduzido para 2 tentativas para maior rapidez
+    max_retries = 2  # Reduzindo o número de tentativas para acelerar
 
     while try_count < max_retries:
         try:
-            print(f"Acessando a publicação: {url}")
+            logger.info(f"Acessando a publicação: {url}")
             driver.get(url)
-            time.sleep(5)  # Tempo para garantir o carregamento da página
 
-            # Rolar a página para garantir que o campo de comentário esteja visível
-            for _ in range(7):
+            # Reduzindo tempo de espera para encontrar o campo de comentário
+            campo_comentario_container = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-e2e="comment-input"]'))
+            )
+
+            for _ in range(3):  # Reduzindo o número de rolagens
                 driver.execute_script("window.scrollBy(0, 300);")
-                time.sleep(random.uniform(0.2, 0.5))  # Menor tempo para agilizar
+                time.sleep(random.uniform(0.3, 0.7))
 
-            campo_editavel = WebDriverWait(driver, 10).until(
+            campo_editavel = WebDriverWait(driver, 8).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, 'div[contenteditable="true"]'))
             )
 
             ActionChains(driver).move_to_element(campo_editavel).click().perform()
             time.sleep(random.uniform(0.5, 1.0))
 
-            for palavra in comentario.split():
-                campo_editavel.send_keys(palavra + " ")
-                time.sleep(random.uniform(0.1, 0.3))  # Menor intervalo entre palavras
-
-            time.sleep(0.5)
+            campo_editavel.send_keys(comentario)
+            time.sleep(1)
             campo_editavel.send_keys(Keys.RETURN)
-            time.sleep(3)  # Reduzido para acelerar
+            time.sleep(3)
 
             # Verificar se o comentário está presente na página após ser enviado
             page_source = driver.page_source
             if comentario in page_source:
-                print(f"Comentário enviado com sucesso: '{comentario}'")
+                logger.info(f"Comentário enviado com sucesso: '{comentario}'")
                 break  # Se o comentário foi enviado, sair do loop de tentativas
             else:
                 raise Exception("Comentário não encontrado após envio. Tentando novamente...")
 
         except Exception as e:
             try_count += 1
-            print(f"Erro ao enviar comentário (tentativa {try_count}): {str(e)}")
+            logger.error(f"Erro ao enviar comentário (tentativa {try_count}): {str(e)}")
             if try_count >= max_retries:
-                print(f"Falha ao enviar o comentário '{comentario}' após {max_retries} tentativas.")
+                logger.error(f"Falha ao enviar o comentário '{comentario}' após {max_retries} tentativas.")
             else:
-                time.sleep(5)  # Espera reduzida antes de tentar novamente
+                time.sleep(5)  # Espera menor antes de tentar novamente
         finally:
-            time.sleep(2)  # Reduzido para acelerar
+            time.sleep(2)
 
 def executar_automacao_por_perfil(perfil, url_publicacao, comentario, progresso_id, total_comentarios):
     try:
@@ -136,7 +141,7 @@ def executar_automacao_por_perfil(perfil, url_publicacao, comentario, progresso_
         progresso_atual = (1 / total_comentarios) * 100
         sio.emit('progresso', {'id': progresso_id, 'progresso': progresso_atual})
     except Exception as e:
-        print(f"Erro no perfil {perfil}: {e}")
+        logger.error(f"Erro no perfil {perfil}: {e}")
     finally:
         if 'driver' in locals():
             driver.quit()
@@ -162,14 +167,14 @@ def iniciar_automacao(tarefa_path):
     total_comentarios = len(perfis)
     progresso_id = random.randint(1000, 9999)  # Gerar um ID único para este ciclo de progresso
 
-    tamanho_lote = 2  # Aumentar para 2 perfis por vez para maior velocidade
+    tamanho_lote = 2  # Processar dois perfis por vez para acelerar o processo
     for i in range(0, len(perfis), tamanho_lote):
         lote_perfis = perfis[i:i + tamanho_lote]
         lote_comentarios = comentarios[i:i + tamanho_lote]
-        print(f"Processando lote: {lote_perfis} com comentários: {lote_comentarios}")
+        logger.info(f"Processando lote: {lote_perfis} com comentários: {lote_comentarios}")
         processar_lote(lote_perfis, lote_comentarios, url_publicacao, progresso_id, total_comentarios)
 
-    print("Automação finalizada para todos os perfis!")
+    logger.info("Automação finalizada para todos os perfis!")
     sio.emit('progresso', {'id': progresso_id, 'progresso': 100})  # Emitir progresso finalizado
 
 if __name__ == "__main__":
